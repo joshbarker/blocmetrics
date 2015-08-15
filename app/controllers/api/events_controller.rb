624b3e4254f361d2b1b4c427e70b9ab30d252b93
joshbarker <abmjosh@gmail.com>
2015-08-15 22:30:08 +0900

    graphs

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -20,21 +20,7 @@ class API::EventsController < ApplicationController
       Event.create(params[:event])
      @event = registered_application.new(params.require(:registered_application).permit(:name,:url))
 
-   
-      # if registered_application
-      # # @event = Event.new(event_params)
-      #   name = params[:event][:name]
-      #   # seed = params[:event][:event]
-      #   @event = registered_application.events.new(name: name)
-
-      #   if @event.save
-      #     render json: @event, status: :created
-      #   else
-      #     render @event.errors, status: :unprocessable_entity
-      #   end
-      # else
-      #   render json: "Unregistered application", status: :unprocessable_entity
-      # end
+  
    end
  
    def show
